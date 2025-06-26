45f78127208d2ba6f2ce29e3cb264bad71d5d1cc
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-06-26 23:16:11 +0200

    Install firebase

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 import '@mantine/core/styles.css';
 import { Button, MantineProvider, Title } from '@mantine/core';
+import { db } from '../infrastructure/firebase/fb.ts';
+import { push, ref } from 'firebase/database'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const addToFirebase = () => {
+	  const countsRef = ref(db, 'counts');
+	  push(countsRef, count)
+	    .then(() => {
+	      console.log('Count added to Firebase:', count);
+	    })
+	    .catch((error) => {
+	      console.error('Error adding count to Firebase:', error);
+	    });
+  }
 
   return (
     <>
       <MantineProvider>
 		  <Title>Monopoly banker</Title>
 		  <Button onClick={() => setCount(count + 1)}>{count}</Button>
+		  <Button onClick={() => addToFirebase()}>Add to firebase</Button>
 	  </MantineProvider>
     </>
   )
---
+++ src/infrastructure/firebase/fb.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, connectDatabaseEmulator } from 'firebase/database';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBIiUgI3piABMMCbNbVwQrS8bzd6PR14cY",
+	authDomain: "biznes-banker.firebaseapp.com",
+	projectId: "biznes-banker",
+	storageBucket: "biznes-banker.firebasestorage.app",
+	messagingSenderId: "691425387120",
+	appId: "1:691425387120:web:93ca7e987c336faacc0ab5"
+};
+
+export const fb = initializeApp(firebaseConfig);
+export const db = getDatabase(fb);
+
+if (import.meta.env.MODE === 'development') {
+	connectDatabaseEmulator(db, 'localhost', 9000);
+}
