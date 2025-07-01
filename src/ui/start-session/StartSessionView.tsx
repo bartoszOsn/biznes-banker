9cf1d5a7f6ae599ea79cf817370c85994210efb2
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-07-01 23:06:19 +0200

    Change session view

--- src/ui/start-session/StartSessionView.tsx
+++ src/ui/start-session/StartSessionView.tsx
@@ -1,6 +1,7 @@
 import { useDomainOfType } from '../../domain/useDomainOfType.ts';
-import { Avatar, Button, Image, Stack, Text, Title } from '@mantine/core';
+import { Avatar, BackgroundImage, Box, Button, Image, Stack, Text, Title } from '@mantine/core';
 import capitalist from '../../assets/capitalist.svg';
+import dollarGrid from '../../assets/dollar-grid.webp';
 import { useCallback } from 'react';
 import { fireMoneyConfetti } from '../util/fireMoneyConfetti.ts';
 
@@ -13,24 +14,32 @@ export function StartSessionView() {
 	}, [domain.startSession]);
 
 	return (
-		<Stack w={'100%'}
-			   h={'100vh'}
-			   px={16}
-			   pt={48}
-			   gap={32}
-			   align={'center'}
-			   style={{
-				   background: 'linear-gradient(45deg,var(--mantine-color-grape-filled) 0%, var(--mantine-color-violet-filled) 100%)'
-			   }}>
-			<Avatar size={'xl'}
-					radius={'md'}
-					variant={'gradient'}
-					color={'red'}>
-				<Image src={capitalist} alt={'Capitalist Logo'}/>
-			</Avatar>
-			<Title c='white' style={{ textTransform: 'uppercase'}}>E-banking</Title>
-			<Text c='white' style={{ textAlign: 'center'}}>Electronic bank for the monopoly game</Text>
-			<Button variant={'gradient'} onClick={onStartSessionClick}>Start session</Button>
-		</Stack>
+		<Box style={{
+			background: 'linear-gradient(45deg,var(--mantine-color-yellow-filled) 0%, var(--mantine-color-orange-filled) 100%)'
+		}}>
+			<Box style={{
+				backgroundImage: `url(${dollarGrid})`,
+				backgroundSize: '140px 140px',
+				backgroundRepeat: 'repeat'
+			}}>
+				<Stack w={'100%'}
+					   h={'100vh'}
+					   px={16}
+					   pt={48}
+					   gap={32}
+					   align={'center'}>
+					<Avatar size={'xl'}
+							radius={'md'}
+							variant={'filled'}
+							color={'red'}>
+						<Image src={capitalist} alt={'Capitalist Logo'}/>
+					</Avatar>
+					<Title c="white" style={{textTransform: 'uppercase'}}>E-banking</Title>
+					<Text c="white" style={{textAlign: 'center'}}>Electronic bank for the monopoly game</Text>
+					<Button variant={'gradient'} gradient={{from: 'red', to: 'pink', deg: 90}} onClick={onStartSessionClick}>💲Start session</Button>
+				</Stack>
+			</Box>
+		</Box>
+
 	);
 }
