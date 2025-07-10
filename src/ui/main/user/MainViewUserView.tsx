052077da50931dd8ad56eb13a0841eb0b9777757
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-07-10 22:52:28 +0200

    1

--- src/ui/main/user/MainViewUserView.tsx
+++ src/ui/main/user/MainViewUserView.tsx
@@ -1,10 +1,13 @@
 import { useDomainOfType } from '../../../domain/useDomainOfType.ts';
-import { Flex, Grid, Stack, Title } from '@mantine/core';
+import { ActionIcon, Flex, Grid, Group, Skeleton, Stack, Title } from '@mantine/core';
 import { MainViewUserViewTransferButton } from './MainViewUserViewTransferButton.tsx';
 import { Money } from '../../util/Money.tsx';
+import { useDisclosure } from '@mantine/hooks';
+import { IconEye, IconEyeClosed } from '@tabler/icons-react';
 
 export function MainViewUserView() {
 	const domain = useDomainOfType('main');
+	const [showMoney, { toggle: toggleShowMoney }] = useDisclosure(false);
 
 	return (
 		<Flex w="100%"
@@ -15,7 +18,18 @@ export function MainViewUserView() {
 			   px="md">
 			<Stack pt='xl' align='center' style={{ flexGrow: 1}}>
 				<Title size='4rem'>
-					<Money amount={domain.balance} />
+					<Group align='baseline' justify='center'>
+						{
+							showMoney
+								? <Money amount={domain.balance} />
+								: <Group gap={0} align='center'>$<Skeleton w='150px' h='50px' mt='sm' /></Group>
+						}
+						<ActionIcon variant='subtle' size='xl' color='gray' onClick={toggleShowMoney}>
+							{
+								showMoney ? <IconEye size='3rem' /> : <IconEyeClosed size='3rem' />
+							}
+						</ActionIcon>
+					</Group>
 				</Title>
 			</Stack>
 			<Stack mah='100%' style={{ overflowY: 'auto', overflowX: 'hidden' }}>
--- stories-and-bugs.md
+++ stories-and-bugs.md
@@ -12,7 +12,7 @@
 - ✅ [Story] - As a banker, I want to be able to setup starting money amount
 - ✅ [Bug] - Transfer to everyone validates the balance only once.
 - ✅ [Bug] - Transfer to banker shows up as "transfer to everyone" in the modal title
-- 🔵 [Story] - As a user I want to be able to hide my money amount
+- ✅ [Story] - As a user I want to be able to hide my money amount
 - ✅ [Story] - As a user I want to have a sound notification when I receive money
 - 🔵 [Bug] - When money is sent to all users, only last user has sound notification.
   - Might be good oportunity to store transfer to all users as one transaction in database.
