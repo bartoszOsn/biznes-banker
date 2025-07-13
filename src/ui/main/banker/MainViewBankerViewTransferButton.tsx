6b9bc2415b471a924e1d1c32a6afa43a2fbf7c53
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-07-13 16:16:52 +0200

    Refactor MainViewBankerViewTransferButton.tsx to use TransferModal

--- src/ui/main/banker/MainViewBankerViewTransferButton.tsx
+++ src/ui/main/banker/MainViewBankerViewTransferButton.tsx
@@ -1,10 +1,9 @@
 import type { User } from '../../../domain/model/User.ts';
 import { userColorToMantine } from '../../../domain/model/UserColor.ts';
-import { Button, Group, Modal, NumberInput, Stack, Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { useCallback, useState } from 'react';
+import { Button } from '@mantine/core';
+import { useCallback } from 'react';
 import { useDomainOfType } from '../../../domain/useDomainOfType.ts';
-import { Money } from '../../util/Money.tsx';
+import { TransferModal, type TransferModalProps } from '../../common/TransferModal.tsx';
 
 export interface MainViewBankerViewTransferButtonProps {
 	transferTo: 'all' | User;
@@ -17,71 +16,35 @@ export function MainViewBankerViewTransferButton({transferTo}: MainViewBankerVie
 		throw new Error('MainViewBankerViewTransferButton can only be used by the banker.');
 	}
 
-	const [opened, {open, close}] = useDisclosure(false);
-	const [amount, setAmount] = useState<number | undefined>(undefined);
-
-	const onClose = useCallback(() => {
-		close();
-		setAmount(undefined);
-	}, [close]);
-
-	const transfer = useCallback((customAmount?: number) => {
-		if (customAmount === undefined) {
-			customAmount = amount;
-		}
-
-		if (customAmount === undefined || customAmount <= 0) {
-			return;
-		}
-
+	const transfer = useCallback((amount: number) => {
 		if (transferTo === 'all') {
-			domain.transferAsBankerToAll(customAmount);
+			domain.transferAsBankerToAll(amount);
 		} else {
-			domain.transferAsBanker(transferTo.id, customAmount);
+			domain.transferAsBanker(transferTo.id, amount);
 		}
-		onClose();
-	}, [domain, onClose, amount, transferTo]);
+	}, [domain, transferTo]);
+
+	const children: TransferModalProps['children'] = (onClick) => {
+		if (transferTo === 'all') {
+			return (
+				<Button variant="gradient" size="xl" onClick={onClick}>
+					All of them
+				</Button>
+			);
+		}
+
+		return (
+			<Button color={userColorToMantine(transferTo.color)} size="xl" onClick={onClick}>
+				{transferTo.name}
+			</Button>
+		);
+	}
 
 	return (
-		<>
-			{
-				transferTo === 'all' && (
-					<Button variant="gradient" size="xl" onClick={open}>
-						All of them
-					</Button>
-				)
-			}
-			{
-				typeof transferTo !== 'string' && (
-					<Button color={userColorToMantine(transferTo.color)} size="xl" onClick={open}>
-						{transferTo.name}
-					</Button>
-				)
-			}
-
-			<Modal opened={opened} onClose={onClose} title={`Transfer to ${typeof transferTo === 'string' ? 'everyone' : transferTo.name}`}>
-				<Stack gap="lg">
-					<form onSubmit={(e) => {transfer(); e.preventDefault();}}>
-						<Stack gap="md">
-						<NumberInput data-autofocus size="lg" prefix="$" thousandSeparator value={amount}
-									 onChange={(v) => setAmount(typeof v !== 'number' ? undefined : Number(v))}/>
-							<Group gap="xs">
-								{
-									domain.presets.map((preset, index) => (
-										<Button key={index} variant="light" size="lg" onClick={() => transfer(preset.amount)}>
-											<Stack gap="0" align="start">
-												<Text size="xs" c="gray">{preset.name}</Text>
-												<Text size="sm" fw="bold"><Money amount={preset.amount}/></Text>
-											</Stack>
-										</Button>
-									))
-								}
-							</Group>
-						</Stack>
-					</form>
-					<Button onClick={() => transfer()}>Transfer</Button>
-				</Stack>
-			</Modal>
-		</>
+		<TransferModal presets={domain.presets}
+					   title={`Transfer to ${transferTo === 'all' ? 'everyone' : transferTo.name}`}
+					   onTransfer={transfer}>
+			{children}
+		</TransferModal>
 	);
 }
