7be19bceee34f1e34e7aff5219935504bdea4e28
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-07-15 17:24:18 +0200

    Remove unused infra

--- src/infrastructure/firebase/selectSessionStarted.ts
+++
@@ -1,8 +0,0 @@
-import type { Observable } from "rxjs";
-import { selectDbValue } from './util/selectDbValue.ts';
-import { getRef } from './util/getRef.ts';
-import { db } from './fb.ts';
-
-export function selectSessionStarted(sessionId: string): Observable<boolean> {
-	return selectDbValue(getRef(db, sessionId, 'started'));
-}
