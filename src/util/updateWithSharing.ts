78be6fe56e24e8764b5702e619238806f856463a
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-09-02 23:26:12 +0200

    prettify updateWithSharing.ts

--- src/util/updateWithSharing.ts
+++ src/util/updateWithSharing.ts
@@ -10,66 +10,71 @@ function isPlainObject(x: unknown): x is Record<string, unknown> {
 	return proto === Object.prototype || proto === null;
 }
 
+function updateWithSharingArray<T>(prev: T[], next: T[]): T[] {
+	const len = next.length;
+	let changed = prev.length !== len;
+	const out: T[] = new Array(len);
+
+	const minLen = Math.min(prev.length, len);
+	for (let i = 0; i < minLen; i++) {
+		const v = updateWithSharing(prev[i], next[i]);
+		out[i] = v;
+		if (!changed && v !== prev[i]) changed = true;
+	}
+
+	for (let i = minLen; i < len; i++) {
+		out[i] = next[i];
+		changed = true;
+	}
+
+	return (changed ? out : prev);
+}
+
+function updateWithSharingPlainObject<T extends Record<string, unknown>>(prev: T, next: T): T {
+	const prevKeys = Object.keys(prev);
+	const nextKeys = Object.keys(next);
+
+	let changed = false;
+	if (prevKeys.length !== nextKeys.length) {
+		changed = true;
+	}
+
+	const out: Record<string, unknown> = {};
+
+	for (const key of nextKeys) {
+		if (Object.prototype.hasOwnProperty.call(prev, key)) {
+			const v = updateWithSharing(prev[key], next[key]);
+			out[key] = v;
+			if (!changed && v !== prev[key]) changed = true;
+		} else {
+			out[key] = next[key];
+			changed = true;
+		}
+	}
+
+	if (!changed) {
+		for (const key of prevKeys) {
+			if (!Object.prototype.hasOwnProperty.call(next, key)) {
+				changed = true;
+				break;
+			}
+		}
+	}
+
+	return (changed ? out : prev) as T;
+}
+
 export function updateWithSharing<T>(prev: T, next: T): T {
 	if (Object.is(prev, next)) return prev;
 
 	if (isPrimitive(prev) || isPrimitive(next)) return next;
 
 	if (Array.isArray(prev) && Array.isArray(next)) {
-		const len = next.length;
-		let changed = prev.length !== len;
-		const out: unknown[] = new Array(len);
-
-		const minLen = Math.min(prev.length, len);
-		for (let i = 0; i < minLen; i++) {
-			const v = updateWithSharing(prev[i], next[i]);
-			out[i] = v;
-			if (!changed && v !== prev[i]) changed = true;
-		}
-
-		for (let i = minLen; i < len; i++) {
-			out[i] = next[i];
-			changed = true;
-		}
-
-		return (changed ? out : prev) as unknown as T;
+		return updateWithSharingArray(prev, next) as T;
 	}
 
 	if (isPlainObject(prev) && isPlainObject(next)) {
-		const prevObj = prev as Record<string, unknown>;
-		const nextObj = next as Record<string, unknown>;
-
-		const prevKeys = Object.keys(prevObj);
-		const nextKeys = Object.keys(nextObj);
-
-		let changed = false;
-		if (prevKeys.length !== nextKeys.length) {
-			changed = true;
-		}
-
-		const out: Record<string, unknown> = {};
-
-		for (const key of nextKeys) {
-			if (Object.prototype.hasOwnProperty.call(prevObj, key)) {
-				const v = updateWithSharing(prevObj[key], nextObj[key]);
-				out[key] = v;
-				if (!changed && v !== prevObj[key]) changed = true;
-			} else {
-				out[key] = nextObj[key];
-				changed = true;
-			}
-		}
-
-		if (!changed) {
-			for (const key of prevKeys) {
-				if (!Object.prototype.hasOwnProperty.call(nextObj, key)) {
-					changed = true;
-					break;
-				}
-			}
-		}
-
-		return (changed ? out : prev) as T;
+		return updateWithSharingPlainObject(prev, next) as T;
 	}
 
 	return next;
